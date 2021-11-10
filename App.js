346863add8cf4148f33460c9230aa5233b2c153c
kalonman <miraclejames166@gmail.com>
2021-11-10 07:40:04 -0800

    first commit

---
+++ App.js
@@ -0,0 +1,96 @@
+// const item = {
+//   fruit: "apple",
+//   animal: "goat",
+//   name: "smith",
+//   age: "30",
+// };
+
+// let num = "50";
+// let count = 50;
+
+// if (num == count) {
+//   console.log(item.animal);
+// } else if (num > count) {
+//   console.log(item.age);
+// } else {
+//   console.log(item.fruit);
+// }
+
+// const battery = 49;
+
+// switch (battery) {
+//   case 50:
+//     console.log("your battery is half way down ");
+//     break;
+//   case 40:
+//     console.log("your battery is at 40%");
+//     break;
+//   case 30:
+//     console.log("your battery is at 30%");
+//     break;
+//   case 20:
+//     console.log("your battery is at 20%");
+//     break;
+//   case 10:
+//     console.log("your battery is low, would like to be charge");
+//     break;
+//   default:
+//     console.log("please charge");
+// }
+
+// let number = 100
+// while (number > 1) {
+
+// }
+
+// for(let i =100; i > 50;number )
+
+// const person = "kane"
+// const age = "30"
+// const address = "No 5 ayo street ikeja lagos"
+
+// const personInfo = `his name is ${person},and his ${age}, and his stay at at ${address }`
+
+// const calc = "when you times 20 by 20 ${20 * 20}";
+
+// const person = "David";
+// const age = "18";
+// const passion = "fashion designer";
+// const country = "Nigeria";
+
+// const personInfo = `his name is ${person}, and his age ${age}, his is a ${passion}, and his is from ${country}`;
+// console.log(personInfo);
+
+// const person = [
+//   "peter",
+//   "anna",
+//   "rice",
+//   "beans",
+//   20,
+//   { friut: "orange" },
+//   ["james"],
+//   false,
+// ];
+
+// console.log()
+
+// let item = person[5];
+// const { friut } = item;
+// console.log(friut);
+
+
+// const person = [
+//     "peter",
+//     "anna",
+//     "rice",
+//     "beans",
+//     20,
+// ]
+const person = ["peter", "anna", "rice", "beans", 20]
+
+const myperson = person.find((item) => {
+    
+}
+)
+// person
+// console.log(person);
